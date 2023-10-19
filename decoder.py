31794ec41312088e5abb7b88b973ceceb7475b60
atik666 <faisal.atik@gmail.com>
2023-10-19 18:45:41 +0000

    "10/19 4"

--- decoder.py
+++ decoder.py
@@ -3,6 +3,40 @@ from torch import nn
 from torch.nn import functional as F
 from attntion import selfAttention
 
+class VAE_AttentionBlock(nn.Module):
+
+    def __init__(self, channels: int):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = selfAttention(1, channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        # x: (Batch_size, Features, Height, Width)
+
+        residue = x
+        
+        n, c, h, w = x.shape
+
+        # (Batch_size, Features, Height, Width) -> (Batch_size, Features, Height * Width)
+        x = x.view(n, c, h * w)
+
+        # (Batch_size, Features, Height * Width) -> (Batch_size, Height * Width, Features)
+        x = x.transpose(-1, -2)
+
+        # (Batch_size, Height * Width, Features) -> (Batch_size, Height * Width, Features)
+        x = self.attention(x)
+
+        # (Batch_size, Height * Width, Features) -> (Batch_size, Features, Height, Width)
+        x = x.transpose(-1, -2)
+
+        # (Batch_size, Features, Height, Width) -> (Batch_size, Features, Height, Width)
+        x = x.view(n, c, h, w)
+
+        x += residue
+
+        return x
+
 class VAE_ResidualBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int):
         super().__init__()
