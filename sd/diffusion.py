bbee4bc4f47878c27762a80b65f740377c8fe234
atik666 <faisal.atik@gmail.com>
2023-10-23 18:05:17 +0000

    10/23 2

--- sd/diffusion.py
+++ sd/diffusion.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import torch
 from torch.nn import nn
 from torch.nn import functional as F
@@ -22,6 +23,121 @@ class TimeEmbedding(nn.Module):
         #(1, 1280)
         return x
     
+class UNET_ResidualBlock(nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int, n_time: int = 1280):
+        super().__init__()
+        self.groupnorm_feature = nn.GroupNorm(32, in_channels)
+        self.conv_feature = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.linear_time = nn.Linear(n_time, out_channels)
+
+        self.groupnorm_merged = nn.GroupNorm(32, out_channels)
+        self.conv_merged = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, feature: torch.Tensor, time: torch.Tensor) -> torch.Tensor:
+        # feature: (Batch_size, In_channel, Height, Width)
+        # time (1, 1280)
+        residue = feature
+
+        feature = self.groupnorm_feature(feature)
+
+        feature = F.silu(feature)
+
+        feature = self.conv_feature(feature)
+
+        time = F.silu(time)
+
+        time = self.linear_time(time)
+
+        merged = feature + time.unsqueeze(-1).unsqueeze(-1)
+
+        merged = self.groupnorm_merged(merged)
+
+        merged = F.silu(merged)
+
+        merged = self.conv_merged(merged)
+
+        return merged + self.residual_layer(residue)
+    
+class UNET_AttentionBlock(nn.Module):
+
+    def __init__(self, n_heads: int, n_embed: int, d_context = 768):
+        super().__init__()
+        channels = n_heads * n_embed
+
+        self.groupnorm = nn.GroupNorm(32, channels, eps=1e-6)
+        self.conv_input = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+        self.layer_norm_1 = nn.LayerNorm(channels)
+        self.attention_1 = selfAttention(n_heads, channels, in_proj_bias=False)
+        self.layer_norm_2 = nn.LayerNorm(channels)
+        self.attention_2 = CrossAttention(n_heads, channels, d_context, in_proj_bias=False)
+        self.layer_norm_3 = nn.LayerNorm(channels)
+        self.linear_geglu_1 = nn.Linear(channels, 4 * channels * 2)
+        self.linear_geglu_2 = nn.Linear(4 * channels, channels)
+
+        self.conv_output = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+    def forward(self, x, context):
+        # x: (Batch_size, Features, Height, Width)
+        # context: (Batch_size, Seq_len, Dim)
+
+        residue_long = x
+
+        x = self.groupnorm(x)
+
+        x = self.conv_input(x)
+
+        n, c, h, w = x.shape
+
+        # (Batch_size, Features, Height, Width) -> (Batch_size, Features, Height * Width)
+        x = x.view(n, c, h * w)
+
+        # (Batch_size, Features, Height * Width) -> (Batch_size, Height * Width, Features)
+        x = x.transpose(-1, -2)
+
+        # Normalization + Self Attention with skip connection
+
+        residue_short = x
+
+        x = self.layer_norm_1(x)
+        self.attention_1(x)
+        x += residue_short
+
+        residue_short = x
+
+        # Normalization + Cross Attention with skip connection
+        x = self.layer_norm_2(x)
+
+        # Cross Attention
+        self.attention_2(x, context)
+
+        x += residue_short
+
+        residue_short = x
+
+        # Normalization + Feed Forward Layer with GeGLU skip connection
+        x = self.layer_norm_3(x)
+
+        x, gate = self.linear_geglu_1(x).chunk(2, dim=-1)
+        x = x * F.gelu(gate)
+
+        x = self.linear_geglu_2(x)
+
+        x += residue_short
+
+        # (Batch_size, Height * Width, Features) -> (Batch_size, Features, Height, Width)
+        x = x.transpose(-1, -2)
+
+        x = x.view(n, c, h, w)
+
+        return self.conv_output(x) + residue_long
+    
 class Upsample(nn.Module):
 
     def __init__(self, channels: int):
@@ -36,7 +152,6 @@ class Upsample(nn.Module):
         x = self.conv(x)
 
         return x
-
 
 class SwitchSequential(nn.Sequential):
 
@@ -121,6 +236,23 @@ class UNET(nn.Module):
             SwitchSequential(UNET_ResidualBlock(640, 320), UNET_AttentionBlock(8, 40)),  
         ])
 
+class UNET_OutputLayer(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, in_channels)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        # x: (Batch_size, 320, Height / 8, Width / 8)
+
+        x = self.groupnorm(x)
+
+        x = F.silu(x)
+
+        x = self.conv(x)
+
+        # (Batch_size, 4, Height / 8, Width / 8)
+        return x
 
 class Diffusion(nn.Module):
 
